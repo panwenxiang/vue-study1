f79f98255b2eb310c63e2f8e8c78c27d64ff4f93
panwenxiangHome <1845037848@qq.com>
2023-04-15 23:19:03 +0800

    添加了parcel，button组件化

---
+++ src/Button.vue
@@ -0,0 +1,29 @@
+<template>
+  <button class="c-button">hi hi</button>
+</template>
+
+<script>
+export default {
+  name: "c-button"
+}
+</script>
+<style lang="less" scoped>
+.c-button{
+  padding: 0 1em;
+  height: var(--button-height);
+  background: var(--button-bg);
+  border-radius: var(--button-border-radius);
+  color: var(--button-color);
+  border: 1px solid var(--button-border-color);
+  &:active{
+    background: var(--button-active-bg);
+  }
+  &:hover{
+    border-color: var(--button-border-color-hover);
+    cursor: pointer;
+  }
+  &:active{
+    background: var(--button-active-color);
+  }
+}
+</style>
